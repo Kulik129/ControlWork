591c4d932245153e980cafc97ba268674c88dd95
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-02-24 12:23:54 +0300

    update

--- Java/AddAnimalInDataBase.java
+++ Java/AddAnimalInDataBase.java
@@ -1,21 +1,46 @@
 package Java;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+
+import java.sql.*;
+import java.util.Scanner;
+
 public class AddAnimalInDataBase {
-    public static void addAnimalInDataBase(String url,String username,String password) {
-        try{
+    public static void addAnimal(String url, String username, String password) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            try (Connection conn = DriverManager.getConnection(url, username, password)){
-                Statement statement = conn.createStatement();
-                int rows = statement.executeUpdate("INSERT pets(`animal`,`name`,`commands`,`date of birth`) " +
-                        "VALUES ('Кошка', 'Эльза','Спит, ест, мяукает','2022-01-29')");
-                System.out.printf("Added %d rows", rows);
+
+            System.out.print("введите животное: ");
+            String animal = scanner.nextLine();
+
+            System.out.print("Введите имя животного: ");
+            String name = scanner.nextLine();
+
+            System.out.print("Введите команды которые знает животное: ");
+            String commands = scanner.nextLine();
+
+            System.out.print("Ведите год-месяц-дату рождения: ");
+            String dateOfBirth = scanner.nextLine();
+
+            try (Connection conn = DriverManager.getConnection(url, username, password)) {
+
+                String sql = "INSERT INTO pets (`animal`,`name`,`commands`,`date of birth`) Values (?, ?, ?, ?)";
+                PreparedStatement preparedStatement = conn.prepareStatement(sql);
+                preparedStatement.setString(1, animal);
+                preparedStatement.setString(2, name);
+                preparedStatement.setString(3, commands);
+                preparedStatement.setString(4, dateOfBirth);
+
+                int rows = preparedStatement.executeUpdate();
+
+                System.out.printf("%d rows added", rows);
             }
-        }
-        catch(Exception ex){
+        } catch (Exception ex) {
             System.out.println("Connection failed...");
             System.out.println(ex);
         }
     }
+
+
+
 }
--- Java/AskAction.java
+++ Java/AskAction.java
@@ -1,24 +1,24 @@
 package Java;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AskAction  {
-//    static void ask() {
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Нажмите 1 если желаете записать новое животное\nНажмите 2 если желаете считать список животных");
-//        int as = in.nextInt();
-//
-//        if (as == 1) {
-//            System.out.println("Введите имя животного, напр.(Мухтар)");
-//            String nameAnimal = in.next();
-//            System.out.println("Введите команды которые питомец знает, напр.(сидеть, фас)");
-//            String knowsCommands = in.next();
-//            AppendInFileOrReadFile.addNewAnimal(nameAnimal, knowsCommands);
-//            ask();
-//        }
-//        if (as == 2) {
-//            AppendInFileOrReadFile.readFile();
-//            ask();
-//        }
-//    }
+    static void ask() throws SQLException {
+    String url = "jdbc:mysql://localhost:3306/Human_Friends";
+    String username = "root";
+    String password = "12345678";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Нажмите 1 если желаете записать новое животное\nНажмите 2 если желаете посмотреть команды которые знает животное");
+        int as = in.nextInt();
+
+        if (as == 1) {
+            AddAnimalInDataBase.addAnimal(url,username,password);
+                ask();
+        }
+        if (as == 2) {
+            GetCommands.outputOfCommands(url,username,password);
+            ask();
+        }
+ }
 }
